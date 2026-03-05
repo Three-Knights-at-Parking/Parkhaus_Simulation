1b15274321f104dbad30a783797bb787d3f047b0
Daniel <d.weimer777@gmail.com>
2026-03-05 21:29:34 +0100

    Function user_input() - pre-validation added: return -1 when input error, invalid input, type over-/underflow

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #include "../include/ui/ui.h"
 
 int user_input(void)
 {
     char buffer[64];
+    char *endptr = NULL;
     long value = 0;
 
     printf("Enter the number (int) you want to navigate to: ");
@@ -15,7 +17,20 @@ int user_input(void)
         return -1;
     }
 
+    errno = 0;
     value = strtol(buffer, &endptr, 10);
+
+    /* No digits were found - endptr points to the first element of buffer*/
+    if (endptr == buffer)
+    {
+        return -1;
+    }
+
+    /* Overflow/underflow */
+    if (errno != 0)
+    {
+        return -1;
+    }
 
     return (int)value;
 }
