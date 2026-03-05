6fb1ed05939a88dccb872391caad62b97248d746
Daniel <d.weimer777@gmail.com>
2026-03-05 21:05:55 +0100

    Function user_input() - implementing first steps to read user_input using fgets

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -1,1 +1,21 @@
-#include "ui/ui.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../include/ui/ui.h"
+
+int user_input(void)
+{
+    char buffer[64];
+    long value = 0;
+
+    printf("Enter the number (int) you want to navigate to: ");
+
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+        return -1;
+    }
+
+    value = strtol(buffer, &endptr, 10);
+
+    return (int)value;
+}
