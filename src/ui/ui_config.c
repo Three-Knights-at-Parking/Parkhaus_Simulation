f8b7b9cd319dc5a95a15c09ab6bb6aafdfc07cb7
Daniel <d.weimer777@gmail.com>
2026-03-06 00:02:45 +0100

    Adding #include to ui_config.c

--- src/ui/ui_config.c
+++ src/ui/ui_config.c
@@ -1,1 +1,11 @@
-#include "ui/ui_config.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <limits.h>
+
+#include "../include/ui/ui.h"
+#include "../include/ui/ui_config.h"
+
+#include "../include/Settings.h"
+#include "../include/types.h"
