f2f2a3aa04745daff46b92b272eba1467f3a0388
maupher <ibach.simon-it25@dhbw-ravensburg.de>
2026-03-06 23:24:26 +0100

    Simulation.c + initialisierung Queue + initialisierung parkhouse
    Signed-off-by: maupher <ibach.simon-it25@dhbw-ravensburg.de>

--- src/Simulation.c
+++ src/Simulation.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 
+#include "Parkhaus.h"
 #include "utils/SafteyUtils.h"
 #include "utils/RNG.h"
 #include "Queue.h"
@@ -11,23 +12,71 @@ int simulation_init(Simulation *p_sim, const Settings *p_settings, const StatLis
     checkNull(p_settings);
     checkNull(stats);
 
-
-
     p_sim->settings = (Settings *) p_settings;
     p_sim->StatList = (StatList *) stats;
     p_sim->current_tick = 0;
 
-    if (rng_init(p_sim->settings) == ERROR)
-    {
-      return ERROR;
-    };
+    int status = OK;
 
+    status = rng_init(p_sim->settings);
+    if (status == ERROR)
+    {
+        return ERROR;
+    }
+
+    //erstellen der Queues
     Queue *Gate_Queues = malloc(p_sim->settings->gates);
 
+    if (Gate_Queues == NULL)
+    {
+        int j = 0;
+        while (j < p_sim->settings->gates)
+        {
+            queue_free(&Gate_Queues[j]);
+            free(&Gate_Queues[j]);
+        }
+        print_error("Gate Queues allocation failed");
+        return ERROR;
+    }
+    //initialisierung der Queues
     for (int i = 0; i < p_sim->settings->gates; i++)
     {
         queue_init(&Gate_Queues[i], -1);
     }
+
+    //Allocation of Parkhouse
+    p_sim->parkhaus = calloc(1, sizeof(Parkhaus));
+    if (p_sim->parkhaus == NULL)
+    {
+        int j = 0;
+        while (j < p_sim->settings->gates)
+        {
+            queue_free(&Gate_Queues[j]);
+            free(&Gate_Queues[j]);
+        }
+        free(p_sim->parkhaus);
+        p_sim->parkhaus = NULL;
+        print_error("Parkhaus allocation failed");
+        return ERROR;
+    }
+
+    status = parkhaus_init(p_sim->parkhaus, p_sim->settings, &Gate_Queues);
+    if (status == ERROR)
+    {
+        //Parkhouse has not been initialised correctly
+        int j = 0;
+        while (j < p_sim->settings->gates)
+        {
+            queue_free(&Gate_Queues[j]);
+            free(&Gate_Queues[j]);
+        }
+        free(p_sim->parkhaus);
+        p_sim->parkhaus = NULL;
+    }
+
+    //initialisierung von Statistik List Objekt
+
+
 
 
 
