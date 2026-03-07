25cb6301c45b9eb95827bbac04981688383e34d0
Luca Perri <business.lucaperri@gmail.com>
2026-03-07 13:25:57 +0100

    Adding GenericVehicle.c

--- src/generic/GenericVehicle.c
+++ src/generic/GenericVehicle.c
@@ -1,1 +1,15 @@
 #include "GenericVehicle.h"
+
+#include <stddef.h>
+
+void generic_vehicle_init(GenericVehicle *p_vehicle, enum ObjectType type, SimulationTickFunction tick_function,
+                          const uint32_t created_at, uint32_t parking_time) {
+    if (p_vehicle == NULL) {
+        return;
+    }
+
+    p_vehicle->base.type = type;
+    p_vehicle->base.tick = tick_function;
+    p_vehicle->created_at_tick = created_at;
+    p_vehicle->leaving_in_ticks = parking_time;
+}
