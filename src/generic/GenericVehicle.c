0a2a648b3545be4f8a49500166cfd7c6be445bf2
maupher <ibach.simon-it25@dhbw-ravensburg.de>
2026-03-07 01:29:15 +0100

    GenericVehicle.h, GenericVehicle.c adding get_space functions
    Signed-off-by: maupher <ibach.simon-it25@dhbw-ravensburg.de>

--- src/generic/GenericVehicle.c
+++ src/generic/GenericVehicle.c
@@ -23,3 +23,23 @@ void generic_vehicle_init(GenericVehicle *p_vehicle,
     p_vehicle->current_slot = 0; //not implemented now
     p_vehicle->current_floor = 0; //not implemented now
 }
+
+int get_vehicle_minimum_space(GenericVehicle *p_vehicle)
+{
+    //if (p_vehicle.type == CAR) //future Feature
+
+    Car *p_car = (Car *) p_vehicle;
+
+    return p_car->minimum_spaces;
+
+}
+
+int get_vehicle_space_needed(GenericVehicle *p_vehicle)
+{
+    //if (p_vehicle.type == CAR) //future Feature
+
+    Car *p_car = (Car *) p_vehicle;
+
+    return p_car->spaces_needed;
+
+}
