d73c812494a09a7618fc41065c46789dad15d702
Daniel <d.weimer777@gmail.com>
2026-03-09 08:16:40 +0100

    Correcting error-handling - freeing of pointers

--- src/main.c
+++ src/main.c
@@ -5,45 +5,52 @@
 #include "../include/Settings.h"
 #include "../include/Simulation.h"
 
-int main() {
-    /* Temporary Solution */
+int main(void)
+{
     Simulation *p_simulation = malloc(sizeof(Simulation));
     if (p_simulation == NULL)
     {
-        printf("malloc (Simulation) failed");
+        printf("malloc (Simulation) failed\n");
         return ERROR;
     }
+
     Settings *p_settings = malloc(sizeof(Settings));
     if (p_settings == NULL)
     {
-        printf("malloc failed");
+        printf("malloc (Settings) failed\n");
+        free(p_simulation);
         return ERROR;
     }
 
     if (settings_init(
-        p_settings,
-                      SETTINGS_DEFAULT_PATH,
-                      SETTINGS_DEFAULT_NAME,
-                      SETTINGS_DEFAULT_CAPACITY,
-                      SETTINGS_DEFAULT_FLOORS,
-                      SETTINGS_DEFAULT_GATES,
-                      SETTINGS_DEFAULT_REAL_EQUIVALENT,
-                      SETTINGS_DEFAULT_OUTPUT_MODE,
-                      SETTINGS_DEFAULT_MAX_TICKS,
-                      SETTINGS_DEFAULT_RAND_SEED,
-                      SETTINGS_DEFAULT_GATE_ENTRY_SEC,
-                      SETTINGS_DEFAULT_TICK_SEC,
-                      SETTINGS_DEFAULT_MAXIMUM_PARKING_TICKS,
-                      SETTINGS_DEFAULT_MINIMUM_PARKING_TICKS,
-                      SETTINGS_DEFAULT_MODE_SELECT,
-                      SETTINGS_DEFAULT_ENTRY_PROB,
-                      SETTINGS_DEFAULT_IS_LEAVABLE
+            p_settings,
+            SETTINGS_DEFAULT_PATH,
+            SETTINGS_DEFAULT_NAME,
+            SETTINGS_DEFAULT_CAPACITY,
+            SETTINGS_DEFAULT_FLOORS,
+            SETTINGS_DEFAULT_GATES,
+            SETTINGS_DEFAULT_REAL_EQUIVALENT,
+            SETTINGS_DEFAULT_OUTPUT_MODE,
+            SETTINGS_DEFAULT_MAX_TICKS,
+            SETTINGS_DEFAULT_RAND_SEED,
+            SETTINGS_DEFAULT_GATE_ENTRY_SEC,
+            SETTINGS_DEFAULT_TICK_SEC,
+            SETTINGS_DEFAULT_MAXIMUM_PARKING_TICKS,
+            SETTINGS_DEFAULT_MINIMUM_PARKING_TICKS,
+            SETTINGS_DEFAULT_MODE_SELECT,
+            SETTINGS_DEFAULT_ENTRY_PROB,
+            SETTINGS_DEFAULT_IS_LEAVABLE
         ) != OK)
     {
+        free(p_settings);
+        free(p_simulation);
         return ERROR;
     }
+
     if (simulation_init(p_simulation, p_settings, NULL) != OK)
     {
+        delete_settings(p_settings);
+        free(p_simulation);
         return ERROR;
     }
 
@@ -51,8 +58,10 @@ int main() {
 
     if (free_simulation(p_simulation) != OK)
     {
+        delete_settings(p_settings);
         return ERROR;
     }
+
     if (delete_settings(p_settings) != OK)
     {
         return ERROR;
