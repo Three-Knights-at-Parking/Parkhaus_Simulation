86f37ab3e44927036baba81e5243dd0ed94eaf51
Daniel <d.weimer777@gmail.com>
2026-03-10 23:35:16 +0100

    Deleting redundant definition

--- include/Settings.h
+++ include/Settings.h
@@ -26,7 +26,6 @@
 #define SETTINGS_DEFAULT_MAX_TICKS (-1)     // -1 represents 1 day of simulation
 #define SETTINGS_MAXIMUM_DAY_TICKS (-365)     // Max one year of simulation
 #define SETTINGS_MAXIMUM_TICKS INT32_MAX     // Max one year of simulation
-#define SETTINGS_DEFAULT_MAX_TICKS (-1)     // -1 represents 1 day of simulation
 
 #define SETTINGS_MINIMUM_PARKING_TICKS 1
 #define SETTINGS_MAXIMUM_PARKING_TICKS INT32_MAX
